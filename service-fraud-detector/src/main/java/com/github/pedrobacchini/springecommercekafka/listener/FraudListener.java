8f79e3cfd0429f72e683cc8d8ebe3b728c15c885
Pedro Bacchini <pedrobacchini@outlook.com>
2021-08-18 22:19:03 -0400

    configure fraud listener to consume record

--- service-fraud-detector/src/main/java/com/github/pedrobacchini/springecommercekafka/listener/FraudListener.java
+++ service-fraud-detector/src/main/java/com/github/pedrobacchini/springecommercekafka/listener/FraudListener.java
@@ -1,26 +1,46 @@
 package com.github.pedrobacchini.springecommercekafka.listener;
 
 import com.github.pedrobacchini.springecommercekafka.domain.Order;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.support.converter.RecordMessageConverter;
-import org.springframework.kafka.support.converter.StringJsonMessageConverter;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
 
 @Service
 public class FraudListener {
 
     private final Logger logger = LoggerFactory.getLogger(FraudListener.class);
 
-    @Bean
-    public RecordMessageConverter other() {
-        return new StringJsonMessageConverter();
+    @KafkaListener(
+            id = "fraudDetector",
+            topics = "ECOMMERCE_NEW_ORDER",
+            containerFactory = "kafkaListenerContainerFactory"
+    )
+    public void consume(ConsumerRecord<String, Order> record) {
+
+        logger.info("__________________________________");
+        logger.info("Processing new order, checking for fraud");
+        logger.info(record.key());
+        logger.info(String.valueOf(record.value()));
+        logger.info(String.valueOf(record.partition()));
+        logger.info(String.valueOf(record.offset()));
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Order order = record.value();
+        if (isFraud(order)) {
+            logger.info("Order is a fraud!!!!!");
+        } else {
+            logger.info("Approved: " + order);
+        }
     }
 
-    @KafkaListener(id = "fraudDetector", topics = "ECOMMERCE_NEW_ORDER")
-    public void consume(Order order) {
-        logger.info("Received: " + order);
+    private boolean isFraud(Order order) {
+        return order.getAmount().compareTo(String.valueOf(new BigDecimal("45000"))) >= 0;
     }
 }
