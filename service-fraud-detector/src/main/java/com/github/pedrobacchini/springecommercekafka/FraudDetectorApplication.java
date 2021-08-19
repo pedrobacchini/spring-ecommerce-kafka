a7322f28258c682803aee022913d706c1c6d6063
Pedro Bacchini <pedrobacchini@outlook.com>
2021-08-18 20:14:10 -0400

    create project and service fraud detector and http ecommerce

---
+++ service-fraud-detector/src/main/java/com/github/pedrobacchini/springecommercekafka/FraudDetectorApplication.java
@@ -0,0 +1,13 @@
+package com.github.pedrobacchini.springecommercekafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudDetectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FraudDetectorApplication.class, args);
+    }
+
+}
---
+++ service-http-ecommerce/src/main/java/com/github/pedrobacchini/springecommercekafka/HttpEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.github.pedrobacchini.springecommercekafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpEcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HttpEcommerceApplication.class, args);
+    }
+
+}
