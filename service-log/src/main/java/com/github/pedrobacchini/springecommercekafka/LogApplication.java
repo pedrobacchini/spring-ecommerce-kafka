82e10ea2c8b3dc96f7b493e57d08e30ac84eb695
Pedro Bacchini <pedrobacchini@outlook.com>
2021-08-20 15:49:40 -0400

    create log service

---
+++ service-log/src/main/java/com/github/pedrobacchini/springecommercekafka/LogApplication.java
@@ -0,0 +1,13 @@
+package com.github.pedrobacchini.springecommercekafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogApplication.class, args);
+    }
+
+}
---
+++ service-log/src/main/java/com/github/pedrobacchini/springecommercekafka/listener/LogListener.java
@@ -0,0 +1,26 @@
+package com.github.pedrobacchini.springecommercekafka.listener;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LogListener {
+
+    private final Logger logger = LoggerFactory.getLogger(LogListener.class);
+
+    @KafkaListener(
+            id = "logService",
+            topicPattern = "ECOMMERCE.*"
+    )
+    public void consume(ConsumerRecord<String, String> record) {
+        logger.info("__________________________________");
+        logger.info("LOG " + record.topic());
+        logger.info(record.key());
+        logger.info(record.value());
+        logger.info(String.valueOf(record.partition()));
+        logger.info(String.valueOf(record.offset()));
+    }
+}
